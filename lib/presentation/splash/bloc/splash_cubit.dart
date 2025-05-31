75febbd667901ef9d4e3d1d24cfa675b90dc1b79
NickSittipon <sittipon5725@gmail.com>
2025-05-31 15:31:49 +0700

    Initial commit: Add splash screen

---
+++ lib/presentation/splash/bloc/splash_cubit.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:movie_app/presentation/splash/bloc/splash_state.dart';
+
+class SplashCubit extends Cubit<SplashState> {
+  SplashCubit() : super(DisplaySplash());
+
+  void appStated() async{
+    await Future.delayed(Duration(seconds: 2));
+    emit(
+      UnAuthenticated()
+      );
+  }
+}
